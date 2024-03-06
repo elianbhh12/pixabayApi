0d2342fc9fb0958b2a6c4268ffd180078a8e29eb
elianbhh12 <elianbhhhgmail.com>
2024-03-06 14:21:31 -0500

    Segunda Actuarlizacion

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const formulario = document.getElementById("formulario");
 const terminoInput = document.getElementById("termino");
 const categoriaInput = document.getElementById("categoria");
 const resultadosDiv = document.getElementById("resultados");
+const modal = document.getElementById("myModal");
+const modalImg = document.getElementById("img01");
+const captionText = document.getElementById("caption");
 
 formulario.addEventListener("submit", async (e) => {
     e.preventDefault();
@@ -9,7 +12,8 @@ formulario.addEventListener("submit", async (e) => {
     const termino = terminoInput.value;
     const categoria = categoriaInput.value;
 
-    const url = `https://pixabay.com/api/?key=42648211-38ca7cdf8831e74a6f63fec3d&q=${termino}&category=${categoria}`;
+    const url = `https://pixabay.com/api/?key=42648211-38ca7cdf8831e74a6f63fec3d&q=${termino}&category=${categoria}&image_type=photo&orientation=horizontal&color=red`;
+    
 
     const respuesta = await fetch(url);
     const datos = await respuesta.json();
@@ -20,20 +24,43 @@ formulario.addEventListener("submit", async (e) => {
         let resultadosHTML = "";
 
         for (const imagen of datos.hits) {
-          resultadosHTML += `
-          <div class="imagen">
-              <a href="${imagen.largeImageURL}">
-                  <img src="${imagen.previewURL}" alt="${imagen.tags}" >
-              </a>
-              
-          </div>
-          `;
-      }
-
+            resultadosHTML += `
+            <div class="imagen">
+                <img src="${imagen.previewURL}" alt="${imagen.tags}" class="rounded-lg" onclick="abrirModal('${imagen.largeImageURL}', '${imagen.tags}', '${imagen.user}', '${imagen.likes}', '${imagen.views}')">
+                
+            </div>
+            `;
+        }
+        
         resultadosDiv.innerHTML = resultadosHTML;
     }
 });
 
-{/* <p>${imagen.tags}</p> */}
+function abrirModal(url, tags, usuario, likes, vistas) {
+    modal.style.display = "block";
+    modalImg.src = url;
 
+    let info = `
+        <p><i class="fas fa-tags"></i><strong>Etiquetas:</strong> ${tags}</p>
+    `;
+    if (usuario) {
+        info += `
+            <p><i class="fas fa-user"></i><strong>Usuario:</strong> ${usuario}</p>
+        `;
+    }
+    if (likes) {
+        info += `
+            <p><i class="fas fa-heart"></i><strong>Likes:</strong> ${likes}</p>
+        `;
+    }
+    if (vistas) {
+        info += `
+            <p><i class="fas fa-eye"></i><strong>Vistas:</strong> ${vistas}</p>
+        `;
+    }
+    captionText.innerHTML = info;
+}
 
+modal.onclick = function() {
+    modal.style.display = "none";
+}
